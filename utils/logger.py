e4d53834cf8678470606a4c1b71effa299cd4b64
Nícolas Oliveira <nicolasfilipeolvr@gmail.com>
2026-07-20 15:51:44 -0300

    Atualizar o logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,10 +1,11 @@
 import logging
 import sys
 
-def setup_logger():
-    logger = logging.getLogger('bot')
+def get_logger(name=None):
+    logger = logging.getLogger(name or 'bot')
     logger.setLevel(logging.INFO)
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)-8s %(name)s %(message)s'))
-    logger.addHandler(handler)
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)-8s %(name)s %(message)s'))
+        logger.addHandler(handler)
     return logger
