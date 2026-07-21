08b68385760fac14408e3734046b7b94c067145a
Nícolas Oliveira <nicolasfilipeolvr@gmail.com>
2026-07-21 13:22:25 -0300

    Atualizar o security.py

--- utils/security.py
+++ utils/security.py
@@ -7,7 +7,7 @@ def load_encryption_key():
     global ENCRYPTION_KEY
     key = os.getenv('ENCRYPTION_KEY')
     if not key:
-        key_file = '/app/data/secret.key'  # dentro de data
+        key_file = '/app/data/secret.key'
         os.makedirs(os.path.dirname(key_file), exist_ok=True)
         if not os.path.exists(key_file):
             key = Fernet.generate_key().decode()
